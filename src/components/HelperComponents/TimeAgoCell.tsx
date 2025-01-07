13a4ad5d2eb8add8d8edfaae1b3fbea4ac79176d
Seedex-io <seedex.team@gmail.com>
2025-01-07 23:34:51 +0100

    Add files via upload

--- src/components/HelperComponents/TimeAgoCell.tsx
+++ src/components/HelperComponents/TimeAgoCell.tsx
@@ -1,7 +1,7 @@
 import { formatDistanceToNow } from 'date-fns';
 
 export const TimeAgoCell = ({ timestamp }: any) => {
-  const timeAgo = formatDistanceToNow(new Date(timestamp), { addSuffix: false });
+  const timeAgo = formatDistanceToNow(new Date(timestamp * 1000), { addSuffix: true }); 
 
   return <div className="token_explorer_pool_created_at">{timeAgo}</div>;
 };
