61a9b09b0e25c22e82d17eaeb3801a9b503ba615
Seedex-io <seedex.team@gmail.com>
2025-01-08 03:35:16 +0100

    Add files via upload

---
+++ src/screens/Trending/columns.tsx
@@ -0,0 +1,181 @@
+import { GridColDef } from '@mui/x-data-grid';
+import formatNumberSmaller, { commaFormatted } from '../../utils/helpers/numberFormat';
+import { TimeAgoCell } from '../../components/HelperComponents/TimeAgoCell';
+import { getChainLogo, getChainNameById } from '../../utils/chains';
+import CopyText from '../../components/CopyText';
+
+export const columns: GridColDef[] = [
+  {
+    field: 'baseSymbol',
+    headerName: 'TOKEN',
+    flex: 2,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className="flex items-center space-x-2 text-white">
+        <div className="relative w-10 h-10">
+          {/* Token logo */}
+          <img
+            src={`https://dd.dexscreener.com/ds-data/tokens/${getChainNameById(params?.row?.chain)}/${params.row.baseHash}.png`}
+            alt="icon"
+            className="w-10 h-10 rounded-full"
+            onError={(e) => {
+              const imgElement = e.target as HTMLImageElement;
+              imgElement.onerror = null;
+              imgElement.style.display = 'none';
+              imgElement.parentElement?.insertAdjacentHTML(
+                'beforeend',
+                `<div class="w-10 h-10 rounded-full bg-gradient-to-r from-[#ba5ae1]/90 to-[#4cc0ec]/90 flex items-center justify-center text-white font-bold">
+                  ${params?.row?.baseSymbol?.charAt(0)}
+                </div>`
+              );
+            }}
+          />
+
+          {/* Chain icon */}
+          <img
+            src={getChainLogo(params?.row?.chain)}
+            alt="chain icon"
+            className="w-4 h-4 rounded-full absolute bottom-0 right-0 bg-black p-[2px]"
+          />
+        </div>
+        <div>
+          <div className="flex items-center space-x-1">
+            <span className="font-bold">
+              {params.row.baseSymbol}
+            </span>
+            <CopyText text={params.row.baseHash} />
+          </div>
+          <div className="text-sm ">
+            {params.row.baseName}
+          </div>
+        </div>
+      </div>
+    ),
+  },
+  {
+    field: 'createdAt',
+    headerName: 'CREATED AT',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className="token_explorer_created_at">
+        <TimeAgoCell timestamp={params.row.createdAt} />
+      </div>
+    ),
+  },
+  {
+    field: 'volume',
+    headerName: '24h VOLUME',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className="token_explorer_volume_usd">
+        <span className="dollar">$</span>
+        {commaFormatted(params.row.volume, 0)}
+      </div>
+    ),
+  },
+  {
+    field: 'transactions',
+    headerName: 'TXNS',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className="token_explorer_volume_usd">
+        {commaFormatted(params.row.transactions, 0)}
+      </div>
+    ),
+  },
+  {
+    field: 'price',
+    headerName: 'PRICE (USD)',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className="token_explorer_price_usd">
+        <span className="dollar">$</span>
+        {formatNumberSmaller(params.row.price, 2)}
+      </div>
+    ),
+  },
+  {
+    field: 'liquidity',
+    headerName: 'LIQUIDITY',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className="token_explorer_liquidity">
+        <span className="dollar">$</span>
+        {commaFormatted(params.row.liquidity, 0)}
+      </div>
+    ),
+  },
+  {
+    field: 'marketCap',
+    headerName: 'MARKET CAP',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className="token_explorer_market_cap">
+        <span className="dollar">$</span>
+        {commaFormatted(params.row.marketCap, 0)}
+      </div>
+    ),
+  },
+  {
+    field: 'fdMarketCap',
+    headerName: 'FD MARKET CAP',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className="token_explorer_fd_market_cap">
+        <span className="dollar">$</span>
+        {commaFormatted(params.row.fdMarketCap, 0)}
+      </div>
+    ),
+  },
+  {
+    field: 'pc5m',
+    headerName: '5M CHANGE',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className={`token_explorer_pc5m ${params.row.pc5m >= 0 ? 'text-green-500' : 'text-red-500'}`}>
+        {formatNumberSmaller(params.row.pc5m)}%
+      </div>
+    ),
+  },
+  {
+    field: 'pc1h',
+    headerName: '1H CHANGE',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className={`token_explorer_pc1h ${params.row.pc1h >= 0 ? 'text-green-500' : 'text-red-500'}`}>
+        {formatNumberSmaller(params.row.pc1h)}%
+      </div>
+    ),
+  },
+  {
+    field: 'pc6h',
+    headerName: '6H CHANGE',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className={`token_explorer_pc6h ${params.row.pc6h >= 0 ? 'text-green-500' : 'text-red-500'}`}>
+        {formatNumberSmaller(params.row.pc6h)}%
+      </div>
+    ),
+  },
+  {
+    field: 'pc24h',
+    headerName: '24H CHANGE',
+    flex: 1,
+    sortable: false,
+    renderCell: (params: any) => (
+      <div className={`token_explorer_pc24h ${params.row.pc24h >= 0 ? 'text-green-500' : 'text-red-500'}`}>
+        {formatNumberSmaller(params.row.pc24h)}%
+      </div>
+    ),
+  },
+];
--- src/screens/Trending/index.tsx
+++ src/screens/Trending/index.tsx
@@ -1,47 +1,232 @@
-import React, { useEffect, useState } from 'react';
-import { getGainerList, getLoserList } from '../../api/tickerApi';
-import Div from '../../components/SimpleComponents/Div';
-import Table from '../../components/Table';
-import './style.css';
-import themeTrendingPage from './theme';
+import { useEffect, useState } from 'react';
 import { BrowserView } from 'react-device-detect';
 import LeftNavbar from '../../components/LeftNavbar';
+import Div from '../../components/SimpleComponents/Div';
 import SearchBar from '../../components/SearchBar';
-export default function TrendingPage(props: any) {
-  const { theme } = props;
-  const [gainers, setGainers] = useState<any>([]);
-  const [loosers, setLoosers] = useState<any>([]);
+import { DataGrid } from '@mui/x-data-grid';
+import { columns } from './columns';
+import { getTrending } from '../../api/tickerApi';
+import './style.css';
+import themeNewPairs from './theme';
+import Filters from './filters';
+import WarningMessage from '../../components/WarningMsg';
 
-  const setGainerList = async () => {
-    getGainerList().then((res) => {
-      setGainers(res);
-    });
+interface Token {
+  chain: number;
+  pairHash: string;
+  baseHash: string;
+  baseSymbol: string;
+  baseName: string;
+  baseIconTs: number;
+  socials: any;
+  verified: any;
+  quoteHash: string;
+  quoteSymbol: string;
+  quoteName: string;
+  quoteIconTs: number;
+  dexHash: string;
+  dexName: string;
+  dexIconTs: number;
+  price: string;
+  createdAt: number;
+  discoveredAt: any;
+  liquidity: string;
+  marketCap: string;
+  fdMarketCap: string;
+  volume: string;
+  transactions: number;
+  makers: number;
+  pc5m: number;
+  pc1h: number;
+  pc6h: number;
+  pc24h: number;
+}
+
+interface FilterCriteria {
+  minVolume: number | null;
+  maxVolume: number | null;
+  minPrice: number | null;
+  maxPrice: number | null;
+  minLiquidity: number | null;
+  maxLiquidity: number | null;
+  minFDV: number | null;
+  maxFDV: number | null;
+  dateRange: { start: string | null; end: string | null };
+  minLast5mChange: number | null;
+  maxLast5mChange: number | null;
+  minLast1hChange: number | null;
+  maxLast1hChange: number | null;
+  minLast6hChange: number | null;
+  maxLast6hChange: number | null;
+  minLast24hChange: number | null;
+  maxLast24hChange: number | null;
+  minTransactions: number | null;
+  maxTransactions: number | null;
+}
+
+export default function TrendingPage(props: any) {
+  const { theme, mobile } = props;
+  const [style, setStyle] = useState<any>({});
+  const [tokens, setTokens] = useState<Token[]>([]);
+  const [filteredTokens, setFilteredTokens] = useState<Token[]>([]);
+  const [isNavbarOpen, setIsNavbarOpen] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [tempFilters, setTempFilters] = useState<FilterCriteria>({
+    minVolume: null,
+    maxVolume: null,
+    minPrice: null,
+    maxPrice: null,
+    minLiquidity: null,
+    maxLiquidity: null,
+    minFDV: null,
+    maxFDV: null,
+    dateRange: { start: null, end: null },
+    minLast5mChange: null,
+    maxLast5mChange: null,
+    minLast1hChange: null,
+    maxLast1hChange: null,
+    minLast6hChange: null,
+    maxLast6hChange: null,
+    minLast24hChange: null,
+    maxLast24hChange: null,
+    minTransactions: null,
+    maxTransactions: null,
+  });
+
+  const [filters, setFilters] = useState<FilterCriteria>(tempFilters);
+
+  useEffect(() => {
+    fetchTokens();
+  }, []);
+
+  const fetchTokens = async () => {
+    setLoading(true);
+    const data = await getTrending();
+    setTokens(data);
+    setFilteredTokens(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (mobile) {
+      setStyle((stl: any) => ({
+        ...stl,
+        width: '100%',
+        padding: '0px',
+      }));
+    }
+  }, [mobile]);
+
+  const matchesFilter = (token: Token, filters: FilterCriteria): boolean => {
+    const {
+      minVolume,
+      maxVolume,
+      minPrice,
+      maxPrice,
+      minLiquidity,
+      maxLiquidity,
+      minLast5mChange,
+      maxLast5mChange,
+      minLast1hChange,
+      maxLast1hChange,
+      minLast6hChange,
+      maxLast6hChange,
+      minLast24hChange,
+      maxLast24hChange,
+      minFDV,
+      maxFDV,
+      dateRange,
+      minTransactions,
+      maxTransactions,
+    } = filters;
+
+    const withinRange = (value: number | null, min: number | null, max: number | null): boolean =>
+      (min === null || (value ?? Number.MIN_VALUE) >= min) && 
+      (max === null || (value ?? Number.MAX_VALUE) <= max);
+
+    return (
+      withinRange(parseFloat(token.volume), minVolume, maxVolume) &&
+      withinRange(parseFloat(token.price), minPrice, maxPrice) &&
+      withinRange(parseFloat(token.liquidity), minLiquidity, maxLiquidity) &&
+      withinRange(token.pc5m, minLast5mChange, maxLast5mChange) &&
+      withinRange(token.pc1h, minLast1hChange, maxLast1hChange) &&
+      withinRange(token.pc6h, minLast6hChange, maxLast6hChange) &&
+      withinRange(token.pc24h, minLast24hChange, maxLast24hChange) &&
+      withinRange(parseInt(token.transactions.toString().replace(/,/g, '')), minTransactions, maxTransactions) &&
+      withinRange(parseFloat(token.fdMarketCap), minFDV, maxFDV) &&
+      (!dateRange.start || new Date(token.createdAt * 1000).getTime() >= new Date(dateRange.start).getTime()) &&
+      (!dateRange.end || new Date(token.createdAt * 1000).getTime() <= new Date(dateRange.end).getTime())
+    );
+  };
+
+  const applyFilters = () => {
+    const filtered = tokens.filter((token) => matchesFilter(token, filters));
+    setFilteredTokens(filtered);
+  };
+
+  useEffect(() => {
+    applyFilters();
+  }, [filters, tokens]);
+
+  const handleToggleNavbar = (open: boolean) => {
+    setIsNavbarOpen(open);
   };
 
   const handleChangeTheme = () => {
     window.location.reload();
   };
-  const setLoserLists = async () => {
-    getLoserList().then((res) => {
-      setLoosers(res);
-    });
+
+  const updateTempFilter = (key: keyof FilterCriteria, value: any) => {
+    setTempFilters((prev) => ({
+      ...prev,
+      [key]: value,
+    }));
   };
 
-  useEffect(() => {
-    setGainerList();
-    setLoserLists();
-  }, [props.mobile]);
+  const handleApplyFilters = () => {
+    setFilters(tempFilters);
+  };
 
   return (
     <>
       <BrowserView>
+        <LeftNavbar onToggle={handleToggleNavbar} />
       </BrowserView>
-      <SearchBar theme={theme} onChangeTheme={handleChangeTheme} />
-      <Div className="trending_page" sx={themeTrendingPage.container}>
-        <Div className="trending_page_title">⚡ Trending </Div>
-        <Table name="Biggest Gainers" data={gainers} />
-        <Table name="Biggest Losers" data={loosers} />
-      </Div>
+      <div className={`${isNavbarOpen ? 'ml-56' : 'ml-[65px]'}`}>
+        <SearchBar theme={theme} onChangeTheme={handleChangeTheme} />
+        <div className="px-3 mt-3">
+          <Filters filters={tempFilters} onFilterChange={updateTempFilter} onApply={handleApplyFilters} />
+          <WarningMessage />
+          <Div style={style} className="border-l-2 border-[#e867ea] rounded-[8px]" sx={themeNewPairs.container}>
+            <div style={{ height: '1050px', overflowY: 'auto' }}>
+              <DataGrid
+                rows={filteredTokens}
+                columns={columns}
+                getRowId={(row: Token) => row.pairHash}
+                disableColumnMenu
+                headerHeight={40}
+                classes={{
+                  columnHeader: 'flex items-center py-2 pl-3 pr-2 sm:py-3 border-r border-[rgb(74,42,80)]/40',
+                  columnHeaderTitleContainer: 'token_explorer_column_header_title_container',
+                  columnHeaders: 'token_explorer_column_headers',
+                  cell: 'token_explorer_cell',
+                  row: 'token_explorer_row',
+                  columnSeparator: 'token_explorer_column_separator',
+                  root: 'token_explorer_table_root',
+                  footerContainer: 'token_explorer_footer_container',
+                }}
+                onCellClick={(params) => {
+                  window.open(`/${params.row.pairHash}-ether`, '_self');
+                }}
+                className="overflow-hidden"
+              />
+              {loading && <div>Loading...</div>}
+            </div>
+          </Div>
+        </div>
+        <div className="h-10"></div>
+      </div>
     </>
   );
 }
