d4f56d4e0b742098f437feb0bc0eaf65e78e46ae
SEEDEX.IO <seedex.team@gmail.com>
2025-01-14 00:30:43 +0100

    Update honeyPot.ts

--- src/api/honeyPot.ts
+++ src/api/honeyPot.ts
@@ -1,7 +1,7 @@
 import { API } from './constants';
 
 export async function scanHoneypot(address: any, chain: string) {
-  const url = `${API}/scanHoneypot?chain=${chain}&token=${address.split('-')[0]}`;
+  const url = `${API}/scan-honeypot?chain=${chain}&token=${address.split('-')[0]}`;
 
   const res = await fetch(url);
   return await res.json();
